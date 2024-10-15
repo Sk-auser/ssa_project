f8df12a25f1a86b32ca4668f984f644d7dc3a7a4
S-kauser <saba.kauser@student.uts.edu.au>
2024-10-15 17:19:34 +1100

    Week 2 update

--- users/views.py
+++ users/views.py
@@ -5,6 +5,17 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your account has been created! You can now log in.")
+            return redirect('users:login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+    
 @login_required(login_url='users:login')
 def user(request):
     return render(request, "users/user.html")
@@ -26,3 +37,4 @@ def logout_view(request):
     logout(request)
     messages.success(request, "Successfully logged out.")
     return redirect('users:login')
+
